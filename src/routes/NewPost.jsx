6a6bcc250041816536d54ea650813a18688a10b7
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-11 16:26:19 -0300

    Requisição de posts

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import blogFetch from "../axios/config";
+
 import "./NewPost.css";
 
 const NewPost = () => {
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
+
+  const createPost = async (e) => {
+    e.preventDefault();
+
+    const post = { title, body, userId: 1 };
+
+    await blogFetch.post("/posts", {
+      body: post,
+    });
+
+    navigate("/");
+  };
+
   return (
     <div className="new-post">
       <h2>Inserir novo Post:</h2>
-      <form>
+      <form onSubmit={(e) => createPost(e)}>
         <div className="form-control">
           <label htmlFor="title">Título:</label>
           <input
@@ -12,6 +34,7 @@ const NewPost = () => {
             name="title"
             id="title"
             placeholder="Digite o título"
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="form-control">
@@ -20,6 +43,7 @@ const NewPost = () => {
             name="body"
             id="body"
             placeholder="Digite o conteúdo"
+            onChange={(e) => setBody(e.target.value)}
           ></textarea>
         </div>
         <input type="submit" value="Criar Post" className="btn" />
