0ebf1f18c0ee3c5e9fc229c66e08545014b27082
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-11 15:15:36 -0300

    posts na home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -15,7 +15,8 @@ const Home = () => {
       );
 
       const data = response.data;
-      console.log(data);
+
+      setPosts(data);
     } catch (error) {
       console.log(error);
     }
@@ -25,7 +26,24 @@ const Home = () => {
     getPosts();
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;
