a47a67dd0cdcc76b73aaa1af311b5be08ac7fc64
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-11 13:26:31 -0300

    react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import Home from "./routes/Home.jsx";
+import NewPost from "./routes/NewPost.jsx";
+import App from "./App.jsx";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <NewPost />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
