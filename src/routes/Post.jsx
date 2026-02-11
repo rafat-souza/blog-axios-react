a0246e14c0e86cca7292fc2523b1964262804e7d
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-11 16:49:01 -0300

    Carregando página de post individual

---
+++ src/routes/Post.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import blogFetch from "../axios/config";
+
+import "./Post.css";
+
+const Post = () => {
+  const { id } = useParams();
+
+  const [post, setPost] = useState({});
+
+  const getPost = async () => {
+    try {
+      const response = await blogFetch.get(`/posts/${id}`);
+
+      const postData = response.data;
+
+      setPost(postData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  return (
+    <div className="post-container">
+      {!post.title ? (
+        <p>Carregando...</p>
+      ) : (
+        <div className="post">
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Post;
